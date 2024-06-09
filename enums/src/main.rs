adbee66ff0a3264f2cb43b0817e6e941ec14050a
t-benedek <t-benedek@lmuc1001236.fritz.box>
2024-06-09 11:47:46 +0200

    first tests with enums implementing message variants

---
+++ enums/src/main.rs
@@ -0,0 +1,27 @@
+#[derive(Debug)]
+enum Message {
+    Quit, 
+    Move {_x: i32, _y: i32}, 
+    Write(String),
+    ChangeColor(i32, i32, i32)    
+}
+
+impl Message {
+    fn call(&self) {
+        println!("This it the message : {:?}", self);
+    }
+}
+
+fn main() {
+    // How to construct these enum variants
+    let quit = Message::Quit;
+    let _move = Message::Move { _x: 10, _y: 20 };
+    let write = Message::Write(String::from("Hello"));
+    let change = Message::ChangeColor(255, 255, 0);
+
+    quit.call();
+    write.call();
+    change.call();
+}
+
+
