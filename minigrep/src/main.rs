861bf0c2ef5e1a9daaa0f70a3947d962e8497069
t-benedek <t-benedek@lmuc1001236.fritz.box>
2024-09-30 18:25:45 +0200

    added command line parsing for minigrep with serach string and file path

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs;
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Config{query, file_path}
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("Query String <{}>\n", config.query);
+    println!("File content: \n{}", contents);
 }
