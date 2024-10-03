21db053210a7d7fb96e21daf0d7cba06f89cb66c
t-benedek <t-benedek@lmuc1001236.fritz.box>
2024-10-03 14:05:28 +0200

    closure can mutably borrow variables

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 fn main() {
     println!("\n******** Closure definition ********");
     closure_fun();
@@ -6,6 +8,12 @@ fn main() {
     println!("\n******** Closure definition ********");
     closure_inference();
     println!("**************************************\n");
+
+    println!("\n******** Closure Borrowing ********");
+    cl_capture_immutable_ref();
+    println!("**************************************\n");
+
+    use_cl_move_thread();
 }
 
 fn closure_inference() {
@@ -25,4 +33,31 @@ fn closure_fun() {
     println!("adding one to 7 with a function : {}", add_one_fn(7));
     println!("adding one to 7 with verbose  closure : {}", add_one_cl_1(7));
     println!("adding one to 7 with short closure : {}", add_one_cl_2(7));
+}
+
+fn cl_capture_immutable_ref() {
+    let mut list = vec![1, 2, 3];
+    println!("Before defining the closure: {:?}", list);
+
+    let mut borrow_mutably = || list.push(7);
+    
+    // The println would also borrow "list" which leads to a compile error 
+    // println!("Before calling the closure: {:?}", list);
+    
+    borrow_mutably();
+
+    // This println is allowed because it occures after the mutable borrow in the closure is out of scope
+    println!("After calling the closure: {:?}", list);
+}
+
+fn use_cl_move_thread() {
+    use std::thread; 
+    let list = vec![1, 2, 3];
+    println!("Before defining the closure: {:?}", list);
+
+    // keyword "move" is needed because thread could outlive the "list" variable
+    thread::spawn(move || {
+        println!("From thread: {:?}", list)
+    }).join().unwrap();
+
 }
