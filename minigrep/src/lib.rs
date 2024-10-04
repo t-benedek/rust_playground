e1ae4bb54d79dbf0ee00c2ada0e92d214fba8872
t-benedek <t-benedek@lmuc1001236.fritz.box>
2024-10-04 13:23:44 +0200

    changed from loop to iterator implementation using filter() which is more focusing on the high level code

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -44,13 +44,21 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+        ) -> Result<Config, &'static str> {
+        // first element of args is "minigrep" so we can skip that
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
     
@@ -77,13 +85,12 @@ pub fn search<'a> (
     query: &str, 
     contents: &'a str
 ) -> Vec<&'a str> {
-    let mut res = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            res.push(line);
-        }
-    }
-    res
+    
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+
 }
 
 pub fn search_case_insensitive<'a> (
@@ -91,14 +98,12 @@ pub fn search_case_insensitive<'a> (
     contents: &'a str
 ) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut res = Vec::new();
+    
+    contents
+        .lines()
+        .filter(|line| line.contains(&query))
+        .collect()
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            res.push(line);
-        }
-    }
-    res
 }
 
 
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,9 +5,8 @@ use minigrep::Config;
 use minigrep::run;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");    
         process::exit(1);
     });
