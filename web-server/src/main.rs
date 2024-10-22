aba65a7db07d06e185002e55e8d6288717917c6a
t-benedek <t-benedek@lmuc1001236.fritz.box>
2024-10-22 18:50:10 +0200

    getting first request from Browser call via HTTP

---
+++ web-server/src/main.rs
@@ -0,0 +1,25 @@
+use std::{
+    io::{prelude::*, BufReader},   
+    net::{TcpListener, TcpStream}, result,
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        
+        handle_conection(stream);
+    }    
+}
+
+fn handle_conection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| line.is_empty())
+        .collect();
+
+    println!("Reuqest: {:#?}", http_request); 
+}
