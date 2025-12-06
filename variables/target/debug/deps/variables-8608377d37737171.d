e1db5bcc698ef9167635c1eb0916ac46b98b4e70
t-benedek <t-benedek@lmuc1001236.fritz.box>
2025-12-06 18:24:31 +0100

    Remove target directory from repository

--- variables/target/debug/deps/variables-8608377d37737171.d
+++
@@ -1,5 +0,0 @@
-/Users/t-benedek/code/rust/rust_playground/variables/target/debug/deps/variables-8608377d37737171: src/main.rs
-
-/Users/t-benedek/code/rust/rust_playground/variables/target/debug/deps/variables-8608377d37737171.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/t-benedek/code/rust/rust_playground/variables/target/debug/variables: /Users/t-benedek/code/rust/rust_playground/variables/src/main.rs
